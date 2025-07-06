f280a7eba3d9ea6362c749a28cc62a54146b90b2
JorjaSeath <1034815@student.sae.edu.au>
2025-07-06 15:28:55 +1000

    Enemy chase player
    Added to 'enemy' script to allow the enemy to chase the player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
+/// <summary>
+/// Allows the enemy to follow/chase the player
+/// </summary>
 public class Enemy : MonoBehaviour
 {
+    [Header("Core Properties")]
+    [SerializeField] private Rigidbody enemy;
+
+    [Header("AI Functionality")]
+    [SerializeField] private GameObject chaseTarget;
+    [SerializeField] private bool isCurrentlyChasing;
+
     public float health;
 
     private void Update()
@@ -16,6 +27,32 @@ public class Enemy : MonoBehaviour
 
         if (health <= 0)
             Debug.Log("The player has killed an enemy!");
+    }
+
+    private void FixedUpdate()
+    {
+        if (isCurrentlyChasing == false)
+        {
+                //Enemy Movement
+            if (enemy.velocity.magnitude < 5)
+            {
+                    //Input.GetAxis("Vertical") ...W/S
+                    //Input.GetAxis("Horizontal") ...A/D
+                enemy.AddForce(Input.GetAxis("Horizontal") * 5, 0, Input.GetAxis("Vertical") * 5);
+            }
+        }
+        else
+        {
+                //AI chasing movement
+                //We need a TARGET (the player)
+                //Move enemy AI towards the target
+            if (enemy.velocity.magnitude < 1)
+            {
+                //AI needs to know the directions from the AI itself to the player
+                //target.position - me.position
+                enemy.AddForce(chaseTarget.transform.position - gameObject.transform.position);
+            }
+        }
     }
 
 }
