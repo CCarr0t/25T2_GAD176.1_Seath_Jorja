3adae1e4f10b02f0c3794cb52ec0edb89eb7a772
JorjaSeath <1034815@student.sae.edu.au>
2025-07-07 19:02:23 +1000

    Enemy damage
    Added an enemy damage dealing script, along with an enemy health script for the enemy damage script to reference off

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -27,6 +28,11 @@ public class Enemy : MonoBehaviour
 
         if (health <= 0)
             Debug.Log("The player has killed an enemy!");
+
+        if (chaseTarget == null)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void FixedUpdate()
